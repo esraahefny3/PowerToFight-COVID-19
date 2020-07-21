fddbed3db8902b295a5f89f245eb2ea6a01d1507
Esraa Hefny <esraahefny3@gmail.com>
2020-07-22 00:31:28 +0200

    PowerToFight COVID-19

---
+++ js/login-modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+ $("#subscribe_btn").click(function(){
+   // show Modal
+   $('#modalSubscription').modal('show');
+ });
+
+ $("#modal_send_btn").click(function(){
+   // show Modal
+   $('#modalSubscription').modal('hide');
+ });
+});
